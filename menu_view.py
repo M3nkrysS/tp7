99848ff5561f2f9261183b1ff7e34115733b51bb
beaudrytinklerl <beaudrytinklerl@educ.college-montreal.qc.ca>
2025-06-02 14:55:05 -0400

    commit

--- menu_view.py
+++ menu_view.py
@@ -8,9 +8,14 @@ class MenuView(arcade.View):
 
     def on_draw(self):
         self.clear()
-        n_g = arcade.Text("New Game", gc.SCREEN_WIDTH / 2, gc.SCREEN_HEIGHT * 0.75, arcade.color.WHITE_SMOKE)
+        n_g = arcade.Text("New Game", gc.SCREEN_WIDTH / 2, gc.SCREEN_HEIGHT * 0.75, arcade.color.WHITE_SMOKE,
+                          anchor_x="center", anchor_y="center")
 
         n_g.draw()
+        color = 0, 0, 0, 0
+        n_g_clickbox = arcade.draw_lrbt_rectangle_filled(gc.SCREEN_WIDTH / 2 - 40, gc.SCREEN_WIDTH / 2 + 40,
+                                                         gc.SCREEN_HEIGHT * 0.75 - 10, gc.SCREEN_HEIGHT * 0.75 + 10,
+                                                         color)
 
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
         if
